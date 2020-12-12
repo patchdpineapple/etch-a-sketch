7cbd996af15b9772725ad07d4558a09c948aa7ab
patchdpineapple <seanpatrick.penaranda@gmail.com>
2020-12-12 17:01:18 +0800

    Done v1

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
-const container = document.querySelector('#container');
-let draw1 = document.createElement('div');
-let draw2 = document.createElement('div');
-let draw3 = document.createElement('div');
-
-draw1.classList.add('drawdiv');
-draw2.classList.add('drawdiv');
-draw3.classList.add('drawdiv');
-
-draw1.addEventListener('hover', () => {draw1.classList.add('hovering');});
+const container = document.querySelector('#grid-container');
+const clear = document.querySelector('#clear');
+const btnClear = document.createElement('div');
+btnClear.textContent = 'erase grid';
+btnClear.classList.add('clearstyle');
+btnClear.addEventListener('click', clearGrid);
 
 
-container.appendChild(draw1);
-container.appendChild(draw2);
-container.appendChild(draw3);
+clear.appendChild(btnClear);
+
+
+
+let item = [];
+
+window.onload = function () {
+    setGrid(16);
+}
+
+
+function setGrid(num) {
+
+    if (num > 100) return;
+
+    item = new Array();
+    let numGrid = num * num;
+    container.setAttribute('style', `grid-template-columns: repeat(${num}, 1fr)`)
+
+
+    for (let x = 0; x < numGrid; x++) {
+        item[x] = document.createElement('div');
+        // item[x].textContent = '' + (x + 1);
+        item[x].classList.add('items');
+        item[x].addEventListener('mouseover', () => { item[x].classList.add('hovering'); });
+        container.appendChild(item[x]);
+
+    }
+
+}
+
+function clearGrid() {
+    let gridno = 0;
+
+    do {
+        gridno = Number(prompt('Drawing erased. Enter grid size(upto 100)', ''));
+    } while (gridno > 100)
+
+    for (let x = 0; x < item.length; x++) {
+        container.removeChild(item[x]);
+    }
+
+    setGrid(gridno);
+
+}
 
